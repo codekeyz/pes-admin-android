4f01c525c67582ced9e95e0d52b164bc55fd7960
Hover Software Solutions <ositachima.co@gmail.com>
2018-12-17 14:05:01 +0000

    Fixed Navigation

--- app/src/main/java/org/afrikcode/pes/activities/HomeActivity.java
+++ app/src/main/java/org/afrikcode/pes/activities/HomeActivity.java
@@ -187,6 +187,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             case "Available Managers":
                 navigationView.setCheckedItem(R.id.nav_managers);
                 break;
+            case "Available Services":
+                navigationView.setCheckedItem(R.id.nav_services);
+                break;
         }
     }
 
