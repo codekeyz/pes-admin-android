efdd865bc8d3596fac0014372194e5014026ca8f
Hover Software Solutions <ositachima.co@gmail.com>
2019-01-21 18:08:22 +0000

    Refactored Database Paths in Contants

--- app/src/main/java/org/afrikcode/pes/Constants.java
+++ app/src/main/java/org/afrikcode/pes/Constants.java
@@ -2,19 +2,17 @@ package org.afrikcode.pes;
 
 public class Constants {
 
-    public static final String ManagersRef = "managers";
+    public static final String ManagersRef = "Managers";
 
-    public static final String AdministratorsRef = "administrators";
+    public static final String BranchesRef = "Branches";
 
-    public static final String BranchesRef = "branches";
+    public static final String ClientsRef = "Clients";
 
-    public static final String ClientsRef = "clients";
+    public static final String TransactionsRef = "Transactions";
 
-    public static final String TransactionsRef = "transactions";
-
-    public static final String YearTimelineRef = "years";
-    public static final String MonthTimelineRef = "months";
-    public static final String WeekTimelineRef = "weeks";
-    public static final String DayTimelineRef = "days";
-    public static final String serviceRef = "services";
+    public static final String YearTimelineRef = "Years";
+    public static final String MonthTimelineRef = "Months";
+    public static final String WeekTimelineRef = "Weeks";
+    public static final String DayTimelineRef = "Days";
+    public static final String serviceRef = "Services";
 }
